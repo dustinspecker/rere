969518759d91cb4b2752679d30d48ea8591bef90
Dustin Specker <DustinSpecker@DustinSpecker.com>
2024-05-13 21:01:10 -0500

    fix(MegaLinter): correct file owners after formatting
    This fixes files that were formatted by MegaLinter to be owned by the
    user instead of root.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,7 +5,9 @@ ENABLE_LINTERS="${ENABLE_LINTERS:-}"
 
 docker run \
 	--env ENABLE_LINTERS="$ENABLE_LINTERS" \
+	--env GROUP_ID="$(id --group)" \
 	--env MEGALINTER_CONFIG=./configs/.mega-linter.yml \
+	--env USER_ID="$(id --user)" \
 	--rm \
 	--volume "$PWD:/tmp/lint:rw" \
 	--volume megalinter-cache:/root/.cache:rw \
