cedd474f69edcafcab303db578a7f00166058d5c
Dustin Specker <DustinSpecker@DustinSpecker.com>
2024-05-15 21:47:58 -0500

    docs: update examples

--- examples_test.go
+++ examples_test.go
@@ -13,6 +13,7 @@ func ExampleRedactWithAllowList() {
 		Password string
 		Key      []byte
 		IsAdmin  bool
+		Groups   []string
 	}
 
 	testUser := user{
@@ -20,23 +21,29 @@ func ExampleRedactWithAllowList() {
 		Password: "super secret",
 		Key:      []byte("another secret"),
 		IsAdmin:  true,
+		Groups:   []string{"users"},
 	}
+	// RedactWithAllowList redacts all strings and []byte by default
+	defaultRedactedUser := rere.RedactWithAllowList(testUser, nil)
+	fmt.Printf("default redacted value: %+v\n", defaultRedactedUser)
+
 	// allowList is matched against case insensitively
-	allowList := []string{"username"}
+	allowList := []string{"username", "groups"}
 	redactedUser := rere.RedactWithAllowList(testUser, allowList)
-	fmt.Printf("redacted string field value: %+v\n", redactedUser)
+	fmt.Printf("redacted value with allow list: %+v\n", redactedUser)
 
 	// RedactWithAllowList will not modify the original value - perfect for logging
 	// RedactWithAllowList does not require a pointer. This is just to help further exemplify the point
 	// that the original value is left unchanged.
 	redactedUserPointer := rere.RedactWithAllowList(&testUser, allowList)
-	fmt.Printf("redacted string field value on pointer to struct: %+v\n", *redactedUserPointer)
+	fmt.Printf("redacted pointer value: %+v\n", *redactedUserPointer)
 	fmt.Printf("original value left unchanged: %+v\n", testUser)
 
 	//nolint:lll // ignore long line length for example output
-	// Output: redacted string field value: {Username:dustin Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true}
-	// redacted string field value on pointer to struct: {Username:dustin Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true}
-	// original value left unchanged: {Username:dustin Password:super secret Key:[97 110 111 116 104 101 114 32 115 101 99 114 101 116] IsAdmin:true}
+	// Output: default redacted value: {Username:REDACTED Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true Groups:[REDACTED]}
+	// redacted value with allow list: {Username:dustin Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true Groups:[users]}
+	// redacted pointer value: {Username:dustin Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true Groups:[users]}
+	// original value left unchanged: {Username:dustin Password:super secret Key:[97 110 111 116 104 101 114 32 115 101 99 114 101 116] IsAdmin:true Groups:[users]}
 }
 
 func ExampleRedactWithDenyList() {
@@ -46,6 +53,7 @@ func ExampleRedactWithDenyList() {
 		Password string
 		Key      []byte
 		IsAdmin  bool
+		Groups   []string
 	}
 
 	testUser := user{
@@ -53,21 +61,27 @@ func ExampleRedactWithDenyList() {
 		Password: "super secret",
 		Key:      []byte("another secret"),
 		IsAdmin:  true,
+		Groups:   []string{"users"},
 	}
+	// RedactWithDenyList redacts nothing by default
+	defaultRedactedUser := rere.RedactWithDenyList(testUser, nil)
+	fmt.Printf("default denied value: %+v\n", defaultRedactedUser)
+
 	// denyList is matched against case insensitively
 	denyList := []string{"password", "Key"}
 	redactedUser := rere.RedactWithDenyList(testUser, denyList)
-	fmt.Printf("redacted string field value: %+v\n", redactedUser)
+	fmt.Printf("redacted value with deny list: %+v\n", redactedUser)
 
 	// RedactWithDenyList will not modify the original value - perfect for logging
 	// RedactWithDenyList does not require a pointer. This is just to help further exemplify the point
 	// that the original value is left unchanged.
 	redactedUserPointer := rere.RedactWithDenyList(&testUser, denyList)
-	fmt.Printf("redacted string field value on pointer to struct: %+v\n", *redactedUserPointer)
+	fmt.Printf("redacted pointer value: %+v\n", *redactedUserPointer)
 	fmt.Printf("original value left unchanged: %+v\n", testUser)
 
 	//nolint:lll // ignore long line length for example output
-	// Output: redacted string field value: {Username:dustin Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true}
-	// redacted string field value on pointer to struct: {Username:dustin Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true}
-	// original value left unchanged: {Username:dustin Password:super secret Key:[97 110 111 116 104 101 114 32 115 101 99 114 101 116] IsAdmin:true}
+	// Output: default denied value: {Username:dustin Password:super secret Key:[97 110 111 116 104 101 114 32 115 101 99 114 101 116] IsAdmin:true Groups:[users]}
+	// redacted value with deny list: {Username:dustin Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true Groups:[users]}
+	// redacted pointer value: {Username:dustin Password:REDACTED Key:[82 69 68 65 67 84 69 68] IsAdmin:true Groups:[users]}
+	// original value left unchanged: {Username:dustin Password:super secret Key:[97 110 111 116 104 101 114 32 115 101 99 114 101 116] IsAdmin:true Groups:[users]}
 }
